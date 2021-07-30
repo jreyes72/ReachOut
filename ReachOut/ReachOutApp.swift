e4041a34e2d732bf01faff7aa0e49b9333000267
Joel Reyes <Jo.reyescs@gmail.com>
2021-07-30 00:31:13 -0500

    Initial Commit

---
+++ ReachOut/ReachOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReachOutApp.swift
+//  ReachOut
+//
+//  Created by Joel Reyes on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ReachOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
